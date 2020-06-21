2f0c42902070962f3b2f0c545332d8b5f4003624
Wu-Tong <wut08@stern.nyu.edu>
2020-06-21 01:35:34 -0400

    initial commit

---
+++ Functional.h
@@ -0,0 +1,28 @@
+#ifndef Functional_hpp
+#define Functional_hpp
+
+class SimpleOption{
+public:
+    //other definitions here
+    int daysToExpiration() const{return m_daysToExpiration;}
+
+    double getInTheMoneyProbability(int numDays, double currentUncerlyingPrice) const ;
+private:
+    int m_daysToExpiration;
+};
+
+class OptionComparison{
+public:
+    OptionComparison(bool directionLess);
+    OptionComparison(const OptionComparison &p);
+    ~OptionComparison();
+    OptionComparison &operator=(const OptionComparison &p);
+
+    bool operator()(const SimpleOption &o1, const SimpleOption &o2);
+private:
+    bool m_directionLess;
+};
+
+
+
+#endif /* Functional_hpp*/
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include "Functional.h"
+#include "Functional.cpp"
+
+using namespace std;
+
+
+int main()
+{
+    test_use_function();
+    return 0;
+}
